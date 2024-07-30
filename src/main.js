fd0d7268552dcbd0fef45e2ac3ea90fe5ed641ac
gustavo.andrade <gustavo.andrade@tray.net.br>
2024-07-30 12:29:14 -0300

    Adicionando vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-// import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import { inject } from '@vercel/analytics';
+const isProductionEnv = import.meta.env.PROD;
+
+inject({
+    mode: isProductionEnv ? 'production' : 'development',
+});
+
+// LOG DE TESTE
+console.log("isProductionEnv: ", isProductionEnv);
 
 createApp(App).mount('#app')
