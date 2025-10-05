01672023abe591d0a549a02426ddc5d21f3a2d3f
Deustavo <gustavo.andrade00@outlook.com>
2025-10-05 09:21:57 -0300

    apenas imagems de terror buuuu

--- components/ImagePolvo.vue
+++ components/ImagePolvo.vue
@@ -2,15 +2,8 @@
     import { computed } from 'vue'; 
 
     import Polvo from '@/assets/img/polvo.png';
-    import PolvoDefault from '@/assets/img/polvo-default.png';
     import PolvoBruxa from '@/assets/img/polvo-bruxa.png';
-    import PolvoCowboy from '@/assets/img/polvo-cowboy.png';
-    import PolvoMarinheiro from '@/assets/img/polvo-marinheiro.png';
-    import PolvoNerd from '@/assets/img/polvo-nerd.png';
-    import PolvoNiver from '@/assets/img/polvo-niver.png';
-    import PolvoPirata from '@/assets/img/polvo-pirata.png';
     import PolvoRealista from '@/assets/img/polvo-realista.png';
-    import PolvoNoel from '@/assets/img/polvo-noel.png';
     import PolvoFred from '@/assets/img/polvo-fred.png';
     import PolvoAbobora from '@/assets/img/polvo-abobora.png';
     import PolvoFrank from '@/assets/img/polvo-frank.png';
@@ -22,24 +15,15 @@
         let selectedImage = Polvo;
 
         const images = [
-            PolvoDefault,
             PolvoBruxa,
-            PolvoCowboy,
-            PolvoMarinheiro,
-            PolvoNerd,
-            PolvoNiver,
-            PolvoPirata,
             PolvoRealista,
-            PolvoNoel,
             PolvoFred,
             PolvoAbobora,
             PolvoFrank,
         ];
-
-        if (Math.random() < 0.4) {
-            const indexImage = Math.floor(Math.random() * images.length);
-            selectedImage =  images[indexImage];
-        }
+        
+        const indexImage = Math.floor(Math.random() * images.length);
+        selectedImage =  images[indexImage];
 
         return selectedImage;
     });
