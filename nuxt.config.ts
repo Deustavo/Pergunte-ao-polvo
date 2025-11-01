e24c2f4c3d06a1121a32f535a07c1fbd7f306df5
Deustavo <gustavo.andrade00@outlook.com>
2025-11-01 18:16:48 -0300

    ajustes nas fontes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,19 @@ export default defineNuxtConfig({
       ],
       link: [
         {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=optional',
+        },
+        {
           rel: 'stylesheet',
           href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css',
           integrity: 'sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==',
